a8e7d39e40f9037c542d431fcabf918d1ca33270
Your Name <you@example.com>
2023-02-27 14:46:59 -0600

    embbeded website

---
+++ index.php
@@ -0,0 +1,28 @@
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Página de buttons</h1>
+    <!DOCTYPE html>
+<html>
+<head>
+	<title>Ejemplo de PHP con ChatGPT</title>
+	<style>
+		body {
+			background-color: black;
+			color: white;
+			font-weight: bold;
+			font-size: 24px;
+			text-align: center;
+			padding-top: 50px;
+		}
+	</style>
+</head>
+<body>
+
+	<h1>Hola Teo</h1>
+	<p>Este es un ejemplo de PHP creado con ChatGPT.</p>
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION["s_usuario"];?></span>
+
+</body>
+</html>
+</div>
+<!--FIN del cont principal-->
